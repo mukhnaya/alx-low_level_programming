db052a4881cef684b4a0d55ca73b50a56cd84828
mukhnaya <mosesmukhanya@gmail.com>
2022-12-08 06:46:47 -0800

    task 4 is about using put to print in c, alternative for printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
 /**
  *main - Print programing is like a multilingual puzzle
  *
- *
  * Return: 0 for this main function
- * /
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
