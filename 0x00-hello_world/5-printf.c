ea9320d938f2f81d1fc596ee4b5f49fe72cb2ab4
mukhnaya <mosesmukhanya@gmail.com>
2022-12-08 06:52:11 -0800

    task 5 printing with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/*main - this function prints using printf
+ *
+ *Return: 0 because the return value is not defined
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
