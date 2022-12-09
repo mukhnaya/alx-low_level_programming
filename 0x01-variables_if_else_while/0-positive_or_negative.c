0dfabade5f306dab181cbbdd5fe2af055acef85f
mukhnaya <mosesmukhanya@gmail.com>
2022-12-09 09:02:26 -0800

    if else implemeted in task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,23 +6,23 @@
  *
  * Return: 0 the function returns 0
  */
-int main (void)
+int main(void)
 {
 	int n;
+
 	srand(time(0));
-	n=rand() - RAND_MAX / 2;
-	
-	if(n>0)
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
 	{
-		printf("%d is positive",n);
+		printf("%d is positive\n", n);
 	}
-	else if(n==0)
+	else if (n == 0)
 	{
-		printf("%d is zero",n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is negative",n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 
