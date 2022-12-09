f435fe4f005dda4a82f083b28f42f75a6d529698
mukhnaya <mosesmukhanya@gmail.com>
2022-12-09 12:06:29 -0800

    printing numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - function entry point
+ *
+ * Return: 0 - returns nothing
+ *
+ */
+
+int main(void)
+{
+	int k;
+
+	k = 0;
+	while (k <= 9)
+	{
+		printf("%d", k);
+		k++;
+	}
+	printf("\n");
+	return (0);
+}
