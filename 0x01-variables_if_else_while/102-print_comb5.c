a261862324f40844dd3368aa1a84d5030d74d0d9
mukhnaya <mosesmukhanya@gmail.com>
2022-12-09 15:27:02 -0800

    revise question 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,12 +11,12 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 99; i++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (j = i + 1; j < 100; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
 			putchar((i / 10) + '0');
-			putchar((j % 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(' ');
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
