10bdfded5bbbb9ed329fed78244aca1567366960
mukhnaya <mosesmukhanya@gmail.com>
2022-12-09 14:38:02 -0800

    task 10 needs more time

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,13 +11,13 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
 			putchar((i % 10) + '0');
 			putchar((j % 10) + '0');
-			if (i == 9 && j == 9)
+			if (i == 8 && j == 9)
 				continue;
 			putchar(',');
 			putchar(' ');
