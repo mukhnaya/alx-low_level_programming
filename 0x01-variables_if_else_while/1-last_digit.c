40a290d4d689f1edcf2bb81ecb192e9998fa0256
mukhnaya <mosesmukhanya@gmail.com>
2022-12-09 10:33:48 -0800

    task 1 about if with logical operations

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - function of the program
+ *
+ * Return: 0 - returns 0
+ */
+
+int main(void)
+{
+	int n;
+	int lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastDigit = n % 10;
+	if (lastDigit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5", n, lastDigit);
+	}
+	else if (lastDigit < 6 && lastDigit != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, lastDigit);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0", n, lastDigit);
+	}
+	return (0);
+}
