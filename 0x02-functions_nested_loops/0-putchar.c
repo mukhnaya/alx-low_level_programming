a7b42da37da0494e4dc942c3dac366ae913b4916
mukhnaya <mosesmukhanya@gmail.com>
2022-12-13 12:37:55 -0800

    task 0 and READMe file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry function
+ *
+ * Return: 0 - returns null value
+ *
+ */
+
+int main(void)
+{
+	write(2, "_putchar\n", 8);
+	return (0);
+}
