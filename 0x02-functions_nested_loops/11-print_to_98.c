634f2a171c0aadaff72296f2426b78c2e50fc387
mukhnaya <mosesmukhanya@gmail.com>
2022-12-14 10:39:45 -0800

    task 11 is updated

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - function to print natural numbers
+ *
+ * @n: is the input character
+ *
+ * Return: Nothing
+ *
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+
+		}
+	}
+}
