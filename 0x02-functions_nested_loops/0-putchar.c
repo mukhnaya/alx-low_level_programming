e4aad4ca8706c05ab6f58eb59150583615ec91ae
mukhnaya <mosesmukhanya@gmail.com>
2022-12-14 04:05:42 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,21 @@
 #include "main.h"
-
 /**
- * main - entry function
- * by Moses Mukhanya creation of header files
- * Return: 0 - returns null value
+ * main - entry point
+ *
+ * Return: 0 - returns no value
  *
  */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar9't');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
