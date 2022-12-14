713f94bb323c284cf0c2932d817b142a3e08e423
mukhnaya <mosesmukhanya@gmail.com>
2022-12-14 07:35:09 -0800

    task 6 is updated

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - returns absoulte value
+ *
+ * @r: is the absolute value input
+ *
+ * Return: 0 value
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (-r);
+	}
+}
