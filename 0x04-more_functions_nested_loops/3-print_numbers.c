3be72052293cd2f2c8b07f35b2f9957d8370724d
mukhnaya <mosesmukhanya@gmail.com>
2022-12-15 15:13:11 +0000

    task 3 updated

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ *
+ * Return: 0 value
+ *
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
