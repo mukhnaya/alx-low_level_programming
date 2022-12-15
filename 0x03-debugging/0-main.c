84bcaf995bbb9651e9bc9e7b1d93dc241fc1f36d
mukhnaya <mosesmukhanya@gmail.com>
2022-12-15 08:46:29 +0000

    debuging task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,8 +2,17 @@
 /**
  * positive_or_negative - pos an neg
  *
+ * @i: is the input
+ *
  * Return: 0
  *
  */
-void positive_or_negative(int i) 
-
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive", i);
+	else if (i == 0)
+		printf("%d is zero", i);
+	else
+		printf("%d is negative", i);
+}
