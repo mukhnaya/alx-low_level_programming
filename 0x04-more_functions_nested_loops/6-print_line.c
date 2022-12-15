0b152629c2ac0ca1b690dc3f54ad1965d201ea17
mukhnaya <mosesmukhanya@gmail.com>
2022-12-15 16:38:54 +0000

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_line - to draw a line
+ *
+ * @n: is the input value
+ * Return: 0 - no value
+ *
+ */
+void print_line(int n)
+{
+	int p;
+
+	p = 0;
+	while (p <= n)
+	{
+		if (n == 0)
+		{
+			_putchar('_');
+			_putchar('\n');
+			break;
+		}
+		else
+		{
+			_putchar('_');
+		}
+		p++;
+		_putchar('\n');
+	}
+}
