770de8be98270a6079b394139e02040caa10b876
mukhnaya <mosesmukhanya@gmail.com>
2022-12-15 13:24:57 +0000

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
- * main - entry point
- *
- * Return: returns no value
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
-
+	
 	return (0);
 }
