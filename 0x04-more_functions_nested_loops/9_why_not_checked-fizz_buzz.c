c15b8525e2d626b1df20a8312338747c0d299b85
mukhnaya <mosesmukhanya@gmail.com>
2022-12-16 01:06:12 +0000

    task 9 updated

--- 0x04-more_functions_nested_loops/9_why_not_checked-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9_why_not_checked-fizz_buzz.c
@@ -9,22 +9,25 @@
 int main(void)
 {
 	int i;
-	char f[] = "Fizz";
-	char b[] = "Buzz";
-	char fb[] = "FizzBuzz";
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i == 100)
-			printf("%s", b);
-		else if ((i % 3 == 0) && (i % 5 == 0))
-			printf("%s ", fb);
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
 		else if (i % 3 == 0)
-			printf("%s ", f);
+		{
+			printf("Fizz ");
+		}
 		else if (i % 5 == 0)
-			printf("%s ", b);
+		{
+			printf("Buzz ");
+		}
 		else
+		{
 			printf("%d ", i);
+		}
 	}
 	printf("\n");
 	return (0);
