fcce5c1e0aa6b27150baf1a965ec06985fff3ed7
mukhnaya <mosesmukhanya@gmail.com>
2022-12-19 18:19:18 +0000

    task 0 is getting update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - to point to a variable
+ *
+ * @n: the pointer value
+ *
+ * return: void
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
 #ifndef moseh
 #define moseh
+void reset_to_98(int *n);
+
 
 
 #endif
