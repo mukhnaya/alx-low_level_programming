4b3e23924dbe7467975bf26ec4ecd4329df0f214
mukhnaya <mosesmukhanya@gmail.com>
2022-12-19 18:27:12 +0000

    task 1 swapping integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - to swap values
+ *
+ * @a: first pointer to the first variable
+ * @b: second pointer
+ *
+ * Return: voide
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
 #ifndef moseh
 #define moseh
 void reset_to_98(int *n);
-
-
+wap_int(int *a, int *b);
 
 #endif
