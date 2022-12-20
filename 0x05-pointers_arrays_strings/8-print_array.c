ea58d0505177236f95d06a6c6597b10cc8760241
mukhnaya <mosesmukhanya@gmail.com>
2022-12-20 01:31:13 +0000

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - print elements of array
+ *
+ * @a: array
+ * @n: number of elements
+ *
+ * Return: 0
+ *
+ */
+void print_array(int *a, int n)
+{
+	int k;
+
+	k = 0;
+	while (k < (n - 1))
+	{
+		printf("%d, ", a[k]);
+		if (k == (n - 1))
+			printf("%d", a[n - 1]);
+		k++;
+	}
+	printf("\n");
+}
