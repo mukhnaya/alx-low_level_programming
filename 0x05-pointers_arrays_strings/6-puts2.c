49aa8bf3cc0f7e9fa0d9ff95e05844c09ff1313e
mukhnaya <mosesmukhanya@gmail.com>
2022-12-20 01:18:55 +0000

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,18 +11,12 @@ void puts2(char *str)
 {
 	int k;
 
-	k = 0;
-	while (str[k] != '\0')
+	for (k = 0; str[k] != '\0'; k++)
 	{
 		if ((k % 2) == 0)
-		{
 			_putchar(str[k]);
-		}
 		else
-		{
 			continue;
-		}
-		k++;
 	}
 	_putchar('\n');
 }
