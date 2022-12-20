45f1087fba1c2e0c97d0c880923a5f50a3617b4a
mukhnaya <mosesmukhanya@gmail.com>
2022-12-20 00:24:06 +0000

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_rev - print in reverse order
+ *
+ * @s: is the input string
+ *
+ * Return: zero
+ *
+ */
+void print_rev(char *s)
+{
+	int counts;
+	int f;
+	int g;
+
+	counts = 0;
+	f = 0;
+	while (s[f] != '\0')
+	{
+		counts++;
+		f++;
+	}
+	g = (counts + 1);
+	while (g >= 0)
+	{
+		_putchar(s[g]);
+		g--;
+	}
+	_putchar('\n');
+}
