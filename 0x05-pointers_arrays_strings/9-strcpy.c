cd99167de7d4c099ab758a36c6ad3aa470e09d29
mukhnaya <mosesmukhanya@gmail.com>
2022-12-20 01:54:36 +0000

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcpy - to copy
+ *
+ * @dest: - copy to where
+ * @src: - copy from where
+ *
+ * Return: dest value
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int k;
+
+	for (k = 0; src[k] != '\0'; k++)
+	{
+		dest[k] = src[k];
+	}
+	return (dest);
+}
