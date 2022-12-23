d86886fa68f93665f8178639571d3677a8421f72
mukhnaya <mosesmukhanya@gmail.com>
2022-12-23 01:49:14 +0000

    task 4 updated

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array - reversing function
+ *
+ * @a: integer arrays
+ * @n: elements swapping
+ *
+ * Return: nothing
+ *
+ */
+void print_array(int *a, int n)
+{
+	int x, y;
+
+	for (x = n - 1; y >= n / 2; y--)
+	{
+		x = a[n - 1 - y];
+		a[n - 1 - y] = a[y];
+		a[y] = x;
+	}
+}
