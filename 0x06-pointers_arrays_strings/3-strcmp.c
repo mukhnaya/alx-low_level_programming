c3fa03be36c73db70386c4af9cec87a86d0ffd7e
mukhnaya <mosesmukhanya@gmail.com>
2022-12-23 01:37:08 +0000

    task 3 updated

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,28 +10,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int f;
-	int k;
-	char mo;
-	int i;
-	int j;
-	int no;
-
-	k = 0;
-	mo = s1[0];
-	f = 1;
-	while (mo != '\0')
+	while ((*s1 && *s2) && (*s1 == *s2))
 	{
-		k++;
-		mo = s1[f++];
+		s1++;
+		s2++;
 	}
-	i = 0;
-	no = s2[0];
-	j = 1;
-	while (no != '\0')
-	{
-		i++;
-		no = s2[j++];
-	}
-	return (k - i);
+	return (*s1 - *s2);
 }
