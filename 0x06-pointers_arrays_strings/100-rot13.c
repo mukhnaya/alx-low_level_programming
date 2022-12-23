42ad76e4c6beaff7d1781d12b196c8401d7cbd3c
mukhnaya <mosesmukhanya@gmail.com>
2022-12-23 02:56:07 +0000

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * rot13 - function for encoding
+ * @pau: input string
+ *
+ * Return: encoded string
+ *
+ */
+char *rot13(char *pau)
+{
+	int x = 0;
+	int y;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+		'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z', 'A', 'B', 'C', 'D', 'E',
+		'F', 'G', 'H', 'I', 'J', 'K',
+		'L', 'M', 'n', 'o', 'p', 'q',
+		'r', 's', 't', 'u', 'v', 'w',
+		'x', 'y', 'z', 'a', 'b', 'c',
+		'd', 'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm'};
+	while (pau[x])
+	{
+		for (y = 0; y < 52; y++)
+		{
+			if (pau[x] == alphabet[y])
+			{
+				pau[x] = rot13key[x];
+				break;
+			}
+		}
+		x++;
+	}
+	return (pau);
+}
