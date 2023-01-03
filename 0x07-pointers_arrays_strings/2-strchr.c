ff3bb7a6a5ad9fa09716418269ae5ebc103789d6
mukhnaya <mosesmukhanya@gmail.com>
2023-01-03 15:50:54 +0000

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - function to search a character in a string
+ * @s: is string to search from
+ * @c: is the character to search from
+ * Return: Null if nothing and pointer to first occurence if found
+ */
+char *_strchr(char *s, char c)
+{
+	int e;
+
+	e = 0;
+	while (s[e] != '\0')
+	{
+		if (s[e] == c)
+		{
+			return (s + 1);
+		}
+		e++;
+	}
+	return ('\0');
+}
