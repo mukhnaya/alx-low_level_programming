8e09f2cd3119ca2c4ae73092bc6cbbe811bda6e7
mukhnaya <mosesmukhanya@gmail.com>
2023-01-03 18:34:48 +0000

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strspn - function to search a string
+ * @s: string to search from
+ * @accept: string to search
+ * Return: the length of string
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int pau;
+	int mose;
+	int length;
+	int p;
+
+	length = 0;
+	for (pau = 0; accept[pau] != '\0'; pau++)
+	{
+		p = 0;
+		for (mose = 0; s[mose] != '\0'; mose++)
+		{
+			if (accept[pau] == s[mose])
+			{
+				length++;
+				p = 1;
+			}
+		}
+		if (p == 0)
+		{
+			return (length);
+		}
+	}
+	return (length);
+}
