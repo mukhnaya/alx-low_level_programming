12dc2334be72b32eceb4d3f2dd7dbe3f7cb4f133
mukhnaya <mosesmukhanya@gmail.com>
2023-01-09 18:17:31 +0000

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main: entry point
+ * main -  entry point
  * @argc: to return count of arguments
  * @argv: to return arguments supplied
  *
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
 	int pau;
 
-	for (pau = 1; pau < argc; pau++)
+	for (pau = 0; pau < argc; pau++)
 	{
 		printf("%s", argv[pau]);
 		printf("\n");
