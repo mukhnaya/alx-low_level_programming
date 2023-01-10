cd093551e1c9fb46e9c9447a16bb09b7a27bd646
mukhnaya <mosesmukhanya@gmail.com>
2023-01-10 02:43:28 +0000

    task 5 still under development

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <cstring.h>
+/**
+ * main - entry point
+ * @argc: to count arguments supplied
+ * @argv: to return supplied arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int pau;
+	int mos;
+
+	mos = 0;
+	if (argc == 1)
+	{
+		printf("Error");
+		printf("\n");
+		return (1);
+	}
+	if (argc > 1)
+	{
+		for (pau = 0; pau < argc; pau++)
+		{
+			if (atoi(argv[pau] < 0))
+			{
+				printf("%d", mos);
+				printf("\n");
+			}
+		}
+	}
+}
