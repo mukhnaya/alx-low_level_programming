c4abc680facb1c76f05d693d89de39892e91d8d2
mukhnaya <mosesmukhanya@gmail.com>
2023-01-11 10:30:27 +0000

    task updated

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * strcat - to concatenate character
+ * 
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+}
