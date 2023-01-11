3b9d5812fac6a9e5f58e8b98b9767a6902689159
mukhnaya <mosesmukhanya@gmail.com>
2023-01-11 18:55:46 +0000

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - duplicate string and return pointer
+ * @str: string to duplicate
+ *
+ * Return: pointer.
+ */
+char *_strdup(char *str)
+{
+	char *newstr;
+
+	newstr = (char *) malloc(sizeof(*str));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		*newstr = *str;
+		return (newstr);
+	}
+}
