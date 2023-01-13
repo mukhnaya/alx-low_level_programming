9e672bec48a1497c5359bbbe7317d9ce0ea15778
mukhnaya <mosesmukhanya@gmail.com>
2023-01-13 02:18:47 +0000

    task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+/**
+ * free_grid - free a 2D array
+ * @grid: 2D array
+ * @height: height of array
+ *
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int pau;
+
+	for (pau = 1; pau < height; pau++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
