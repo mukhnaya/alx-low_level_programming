9a0149fad80c39439636d7863ca6d53ce53a486e
mukhnaya <mosesmukhanya@gmail.com>
2023-01-14 00:26:11 +0000

    task 0 updated

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,17 +3,20 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * malloc_checked
+ * malloc_checked - check malloc
  * @b: is the size of memory
  *
  * Return: 0
  */
 void *malloc_checked(unsigned int b)
 {
-	int *p = (int *) malloc(b * sizeof(int));
-
+	int *p;
+	
+	p = (int *) malloc(b * sizeof(int));
 	if (p == NULL)
 	{
 		exit(98);
 	}
+	free(p);
+	return (0);
 }
