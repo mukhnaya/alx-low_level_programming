e203e491ef2f6c9dc46fab4897c5cbd35f1c4a11
mukhnaya <mosesmukhanya@gmail.com>
2023-01-17 16:55:28 +0000

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "_putchar.c"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+	printf("%s", __FILE__);
+	printf("\n");
+	return (0);
+}
