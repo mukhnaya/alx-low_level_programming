1c4fb8609ac1dee59d932f4eebb7945e585d94da
mukhnaya <mosesmukhanya@gmail.com>
2023-01-17 20:06:24 +0000

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ *
+ * Description: to hold dog identification details
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
