abec0264de6c245b06f62ea9908129878ebeb7ef
mukhnaya <mosesmukhanya@gmail.com>
2023-01-30 20:14:07 +0000

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_list - frees a list_t list
+ * @head: head
+ * Return: empty list
+ */
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		free(head->str);
+		free(head);
+		head = head->next;
+	}
+}
