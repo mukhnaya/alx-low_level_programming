92dd1617f90d13f76719bcc06329098266e9b2f2
mukhnaya <mosesmukhanya@gmail.com>
2023-01-30 19:40:37 +0000

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - to return number of nodes
+ * @h: is the head of the linked list
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int nodes;
+
+	nodes = 0;
+	while (h != NULL)
+	{
+		nodes++;
+	}
+	return (nodes);
+}
