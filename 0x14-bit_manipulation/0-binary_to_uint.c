a129743f0365f02a4023c581595515ac3a62a800
mukhnaya <mosesmukhanya@gmail.com>
2023-02-07 00:29:51 +0000

    task 0 updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include "main.h"
 /**
  * binary_to_uint - converts binary to unsigned in
  * @b: pointing to 0 and 1
@@ -9,14 +11,27 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int val;
-	int i;
+	unsigned int i;
+	unsigned int mos;
 
+	val = 0;
+	mos = 1;
+	i = 0;
 	if (b == NULL)
-		return (0);
-	while (b[i] == '0' || b[i])
 	{
+		return (0);
+	}
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
 		val <<= 1;
-		val += b[i] - '0';
+		if (b[i] == '1')
+		{
+			val ^= mos;
+		}
 		i++;
 	}
 	return (val);
