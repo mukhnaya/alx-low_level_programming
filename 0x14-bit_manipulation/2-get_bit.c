b97effa0bb2d1f2466dc70e3fad94436284d23ca
mukhnaya <mosesmukhanya@gmail.com>
2023-02-07 00:51:19 +0000

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * get_bit - return bit at index
+ * @n: is the integer value
+ * @index: the index
+ * Retunr: bit value
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mos;
+
+	mos = 1;
+	if (index > sizeof(n) * 8)
+	{
+		return (-1);
+	}
+	mos <<= index;
+	if (mos &  n)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
