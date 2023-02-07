75ffde2ae27b4edeecf063466886dd4dc267518c
mukhnaya <mosesmukhanya@gmail.com>
2023-02-07 08:07:03 +0000

    upated task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,28 +2,19 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * print_list - to return elements of list
+ * print_listint - to return elements of list
  * @h: head of the linked list
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
 	unsigned int pau;
-	const listint_t *mos = NULL;
 
 	pau = 0;
-	mos = h;
-	while (mos != NULL)
+	while (h != NULL)
 	{
-		if (mos->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%d] %s\n", mos->len, mos->str);
-		}
-		mos = mos->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		pau++;
 	}
 	return (pau);
