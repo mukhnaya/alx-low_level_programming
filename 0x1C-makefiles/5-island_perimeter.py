c209047ab732b6590f3d1d16997b8ff87c833cfb
mukhnaya <mosesmukhanya@gmail.com>
2023-05-25 01:12:10 +0000

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,14 +6,14 @@ def island_perimeter(grid):
     '''function to compute perimeter of an island'''
     width = len(grid[0])
     height = len(grid)
-    size = 0
+    perimeter = 0
 
     for mos in range(height):
         for pau in range(width):
             if grid[mos][pau]:
-                size += 4
+                perimeter += 4
                 if (mos > 0 and grid[mos - 1][pau] == 1):
-                    size -= 2
+                    perimeter -= 2
                 if (pau > 0 and grid[mos][pau - 1] == 1):
-                    size -= 2
-    return size
+                    perimeter -= 2
+    return perimeter
